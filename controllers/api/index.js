c8b770c6b16f665813caf350f52eb1510bdb95c3
austin green <trentgr33n@icloud.com>
2022-07-21 20:56:06 -0400

    seeds for review/ need to make post route to get data for review

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
-const router = require("express").Router();
+const router = require('express').Router();
 
-router.get("/", (req, res) => {
-    res.send("in api routes")
-})
+const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes.js');
 
-module.exports = router
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
+
+module.exports = router;
