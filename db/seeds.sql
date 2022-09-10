67f7120c48e4ca667bb15338afc0b85a505a26ea
Seeker <54086931+Seeker-09@users.noreply.github.com>
2022-09-10 18:40:54 -0400

    Update seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,7 @@
-INSERT INTO reviews (id, review_int)
-VALUES 
-  (1, "terrible, worst ever"),
-  (2,"bad"),
-  (3,"ok"),
-  (4,"better than the last one i tried"),
-  (5, "the best ever");
+-- INSERT INTO reviews (id, review_int)
+-- VALUES 
+--   (1, "terrible, worst ever"),
+--   (2,"bad"),
+--   (3,"ok"),
+--   (4,"better than the last one i tried"),
+--   (5, "the best ever");
