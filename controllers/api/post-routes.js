d86ab6d134907ede181e649c41991e4ca1bcec9d
Seeker <54086931+Seeker-09@users.noreply.github.com>
2022-09-10 18:30:07 -0400

    trying something

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -4,48 +4,48 @@ const { Post, User } = require("../../models")
 
 
 // get all posts
-router.get('/', (req, res) => {
-    Post.findAll({
-        include: [
-            {
-                model: User,
-                attributes: ["username"]
-            }
-        ]
-    })
-    .then(dbPostData => res.json(dbPostData))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err)
-    })
-})
+// router.get('/', (req, res) => {
+//     Post.findAll({
+//         include: [
+//             {
+//                 model: User,
+//                 attributes: ["username"]
+//             }
+//         ]
+//     })
+//     .then(dbPostData => res.json(dbPostData))
+//     .catch(err => {
+//         console.log(err);
+//         res.status(500).json(err)
+//     })
+// })
 
 // get post by id
-router.get("/:id", (req, res) => {
-    Post.findOne({
-        where: {
-            id: req.params.id
-        },
-        include: [
-            {
-                model: User,
-                attributes: ["username"]
-            }
-        ]
-    })
-    .then(dbPostData => {
-        // check if post exists
-        if(!dbPostData) {
-            res.status(404).json({message: "No post found with this id"})
-            return
-        }
-        res.json(dbPostData)
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err)
-    })
-})
+// router.get("/:id", (req, res) => {
+//     Post.findOne({
+//         where: {
+//             id: req.params.id
+//         },
+//         include: [
+//             {
+//                 model: User,
+//                 attributes: ["username"]
+//             }
+//         ]
+//     })
+//     .then(dbPostData => {
+//         // check if post exists
+//         if(!dbPostData) {
+//             res.status(404).json({message: "No post found with this id"})
+//             return
+//         }
+//         res.json(dbPostData)
+//     })
+//     .catch(err => {
+//         console.log(err);
+//         res.status(500).json(err)
+//     })
+// })
 
 // Create a post
 router.post("/", (req, res) => {
